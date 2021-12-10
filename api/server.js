e7f38f86641b1ffcd91603bb30197844dfac1d99
Raj-04 <1204patel.raj@gmail.com>
2021-12-10 16:33:35 -0500

    error handling and the server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
-// build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res, next) => {
+  next({
+    status: 404,
+    message: 'This endpoint does not exist'
+  })
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.message
+  })
+})
+
+module.exports = server
